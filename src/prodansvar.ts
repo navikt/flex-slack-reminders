90999d46082d0f14d30940232c3e02a5d5f7ddf3
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-08-28 15:37:49 +0200

    prodansvar

--- src/prodansvar.ts
+++ src/prodansvar.ts
@@ -22,13 +22,30 @@ console.log(`Ansvarlig er ${ansvarlig.initialer}`)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const blocks = [] as any[]
 
-blocks.push({
-    type: 'section',
-    text: {
-        type: 'mrkdwn',
-        text: `:male-police-officer: *Prodansvar denne uka*
-Det er <@${ansvarlig.memberId}> som har prodansvar denne uka.`,
+blocks.push(
+    {
+        type: 'divider',
     },
-})
+    {
+        type: 'section',
+        text: {
+            type: 'mrkdwn',
+            text: `:male-police-officer: *Prodansvar denne uka*
+Det er <@${ansvarlig.memberId}> som har prodansvar denne uka.`,
+        },
+    },
+    {
+        type: 'section',
+        text: {
+            type: 'mrkdwn',
+            text: `:link: *Nyttige lenker*
+<https://logs.adeo.no/goto/85280c10-59cf-11ed-8607-d590fd125f80|Logger flex namespace>
+<https://grafana.nais.io/d/H8jR8MFGk/flex-kafka-lag?orgId=1&refresh=5m&from=now-26h&to=now&var-datasource=prod-gcp|Flex kafka lag dashboardet>
+<https://grafana.nais.io/d/AdtqLupWk/spoknad-funksjonell?orgId=1&from=now-1d%2Fd&to=now-1d%2Fd&refresh=1m&var-datasource=prod-gcp&var-namespace=All|Spøknad funksjonell>
+<https://grafana.nais.io/d/AVwFIm0Mz/cloudsql-gcp?orgId=1&var-datasource=prod-gcp-stackdriver&var-project=nais-prod|CloudSQL - GCP>
+<https://github.com/orgs/navikt/security/alerts/dependabot?q=is:open%20team:flex|Github sårbarheter>`,
+        },
+    },
+)
 
 await sendSlackMessage('FLEX_DEV_WEBHOOK', { blocks })
