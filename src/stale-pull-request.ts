0a1dbf8e462e5cd0d6505b6de7d5357b3152a5a6
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-09-25 13:50:26 +0200

    Kompakt post

--- src/stale-pull-request.ts
+++ src/stale-pull-request.ts
@@ -8,6 +8,8 @@ import { numberToSlackEmoji } from './common/numberToEmoji'
 
 const repoer = hentRepoer()
 const antallDager = 7
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const blocks = [] as any[]
 
 for (const repo of repoer) {
     console.log(`Sjekker repo '${repo}'`)
@@ -17,22 +19,19 @@ for (const repo of repoer) {
     })
     const gamle = pulls.data.filter((pull) => dayjs().diff(dayjs(pull.created_at), 'day') > antallDager)
     if (gamle.length > 0) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const blocks = [] as any[]
-        blocks.push({ type: 'divider' })
-
         blocks.push({
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: `:warning: * <https://www.github.com/navikt/${repo}|${repo}>*
+                text: `
+
+:warning: * <https://www.github.com/navikt/${repo}|${repo}>*
 Har totalt <https://www.github.com/navikt/${repo}/pulls|${pulls.data.length} pull requests>. ${gamle.length} av dem er eldre enn ${antallDager} dager.
 Vi bør merge eller lukke disse`,
             },
         })
 
         gamle.map((pull, idx) => {
-            blocks.push({ type: 'divider' })
             blocks.push({
                 type: 'section',
                 text: {
@@ -57,8 +56,25 @@ Vi bør merge eller lukke disse`,
                 ],
             })
         })
-
-        await sendSlackMessage('FLEX_DEV_WEBHOOK', { blocks })
-        console.log(`Sendte til slack for repo '${repo}'`)
     }
+}
+
+//splitt i blokker på 50 og post til slack
+const chunkSize = 50
+const chunkedBlocks = blocks.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / chunkSize)
+
+    if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = [] // start a new chunk
+    }
+
+    resultArray[chunkIndex].push(item)
+
+    return resultArray
+}, [])
+
+// post chunked blocks to slack
+for (const blocks of chunkedBlocks) {
+    const res = await sendSlackMessage('FLEX_DEV_WEBHOOK', { blocks })
+    console.log(`Sendte til slack: ${res}'`)
 }
