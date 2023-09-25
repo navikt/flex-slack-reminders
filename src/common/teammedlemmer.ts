d5b5512943d57a4886c287459fe3481db0d45f24
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-09-25 14:57:55 +0200

    Endre rekkefølgen

--- src/common/teammedlemmer.ts
+++ src/common/teammedlemmer.ts
@@ -1,13 +1,17 @@
 export const prodansvarlige = [
-    { initialer: 'HSA', memberId: 'UPU6U4H9R' },
     { initialer: 'BIK', memberId: 'UMWEVMRM4' },
     { initialer: 'NJM', memberId: 'U02AM04QV96' },
+    { initialer: 'HSA', memberId: 'UPU6U4H9R' },
     { initialer: 'ØK', memberId: 'U021UJDDMHB' },
     { initialer: 'GB', memberId: 'U0124V94CLR' },
 ]
 
 export const flexjaransvarlige = [
-    ...prodansvarlige.reverse(),
+    { initialer: 'HSA', memberId: 'UPU6U4H9R' },
+    { initialer: 'BIK', memberId: 'UMWEVMRM4' },
+    { initialer: 'NJM', memberId: 'U02AM04QV96' },
+    { initialer: 'ØK', memberId: 'U021UJDDMHB' },
+    { initialer: 'GB', memberId: 'U0124V94CLR' },
     { initialer: 'PB', memberId: 'UE89E1WTY' },
     { initialer: 'MG', memberId: 'UJW0A57FD' },
 ]
