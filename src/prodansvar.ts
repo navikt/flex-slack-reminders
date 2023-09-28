03df695c3e1a35df80e04c00c4a0ac4b2528c289
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-09-28 16:26:43 +0200

    Lenker

--- src/prodansvar.ts
+++ src/prodansvar.ts
@@ -34,9 +34,10 @@ Det er <@${ansvarlig.memberId}> som har prodansvar denne uka.`,
             text: `:link: *Nyttige lenker*
 <https://logs.adeo.no/goto/85280c10-59cf-11ed-8607-d590fd125f80|Logger flex namespace>
 <https://grafana.nais.io/d/H8jR8MFGk/flex-kafka-lag?orgId=1&refresh=5m&from=now-26h&to=now&var-datasource=prod-gcp|Flex kafka lag dashboardet>
-<https://grafana.nais.io/d/AdtqLupWk/spoknad-funksjonell?orgId=1&from=now-1d%2Fd&to=now-1d%2Fd&refresh=1m&var-datasource=prod-gcp&var-namespace=All|Spøknad funksjonell>
+<https://grafana.nais.io/d/AdtqLupWk/flex-funksjonell?from=now-24h&to=now|Flex funksjonell>
 <https://grafana.nais.io/d/AVwFIm0Mz/cloudsql-gcp?orgId=1&var-datasource=prod-gcp-stackdriver&var-project=nais-prod|CloudSQL - GCP>
 <https://github.com/orgs/navikt/security/alerts/code-scanning?query=is:open%20team:flex|Code QL funn>
+<https://grafana.nais.io/d/CK_g1AWIk/flex-app-oversikt?orgId=1&var-datasource=prod-gcp&var-namespace=flex&var-app=All&var-environment=prod|Flex apper oversikt>
 <https://github.com/orgs/navikt/security/alerts/dependabot?q=is:open%20team:flex|Github sårbarheter>`,
         },
     },
