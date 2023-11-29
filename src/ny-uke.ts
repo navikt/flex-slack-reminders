4498888bba6604b5adfc531efc5c85d865385896
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-29 15:09:48 +0100

    Ny uke

--- src/ny-uke.ts
+++ src/ny-uke.ts
@@ -2,46 +2,48 @@ import './common/configInit'
 import * as dayjs from 'dayjs'
 import * as weekOfYear from 'dayjs/plugin/weekOfYear'
 
-import { sendSlackMessage } from './common/slackPosting'
 import { prodansvarlig } from './common/prodansvarlig'
 import { flexjaransvarlig } from './common/flexjaransvarlig'
+import { slackWebClient } from './common/slackClient'
+import { flexInternal } from './common/slackChannels'
 
 dayjs.extend(weekOfYear)
 
 dayjs('2018-06-27').week() // 26
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const blocks = [] as any[]
-
-blocks.push(
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `:wave: Det er ukenummer ${dayjs().week()} `,
+await slackWebClient.chat.postMessage({
+    channel: flexInternal(),
+    blocks: [
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `:wave: Det er ukenummer ${dayjs().week()} `,
+            },
         },
-    },
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `OKR Boardet finner vi her <https://flex-docs.ekstern.dev.nav.no/okr-board|OKR Board>`,
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `OKR Boardet finner vi her <https://flex-docs.ekstern.dev.nav.no/okr-board|OKR Board>`,
+            },
         },
-    },
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `Det er <@${prodansvarlig().memberId}> som har prodansvar hele denne uka.`,
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `Det er <@${prodansvarlig().memberId}> som har prodansvar hele denne uka.`,
+            },
         },
-    },
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `På fredag har <@${flexjaransvarlig().memberId}> flexjaransvar.`,
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `På fredag har <@${flexjaransvarlig().memberId}> flexjaransvar.`,
+            },
         },
-    },
-)
-
-await sendSlackMessage('FLEXINTERNAL_WEBHOOK', { blocks })
+    ],
+    icon_emoji: ':godstolen:',
+    username: 'Ny uke for Team Flex',
+    text: 'Ny uke',
+})
