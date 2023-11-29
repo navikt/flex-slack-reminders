23b6bf7a21527b9c73165f4b5a0a20835d92b7ab
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-29 14:51:54 +0100

    Bruk tråd

--- src/flexjaransvar.ts
+++ src/flexjaransvar.ts
@@ -7,39 +7,44 @@ import { flexInternal } from './common/slackChannels'
 const ansvarlig = flexjaransvarlig()
 console.log(`Ansvarlig er ${ansvarlig.initialer}`)
 
-// eslint-disable-next-line
-const blocks = [] as any[]
-
-blocks.push(
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `Det er <@${ansvarlig.memberId}> som har flexjar ansvar denne uka.`,
-        },
-    },
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `Teamet har et rullerende flexjaransvar blant alle medlemmene. Ansvaret er å fjerne siste ukes feedback med personopplysninger. Man skal også poste en kurert post på fredager i #flex-innsikt
-\n\nMan inkluderer da forrige fredag, lørdag og søndag i sin uke.`,
-        },
-    },
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `:link: *Nyttige lenker*
-<https://flexjar.intern.nav.no/|Flexjar>`,
-        },
-    },
-)
-
-await slackWebClient.chat.postMessage({
+const hovedpost = await slackWebClient.chat.postMessage({
     channel: flexInternal(),
-    blocks,
+    blocks: [
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `Det er <@${ansvarlig.memberId}> som har flexjar ansvar denne uka.`,
+            },
+        },
+    ],
     icon_emoji: ':prinsipp_brukers_situasjon:',
     username: 'Flexjar ansvar denne uka',
     text: 'Ukens flexjaransvar',
+})
+
+await slackWebClient.chat.postMessage({
+    channel: flexInternal(),
+    blocks: [
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `Teamet har et rullerende flexjaransvar blant alle medlemmene. Ansvaret er å fjerne siste ukes feedback med personopplysninger. Man skal også poste en kurert post på fredager i #flex-innsikt
+\n\nMan inkluderer da forrige fredag, lørdag og søndag i sin uke.`,
+            },
+        },
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `:link: *Nyttige lenker*
+<https://flexjar.intern.nav.no/|Flexjar>`,
+            },
+        },
+    ],
+    icon_emoji: ':prinsipp_brukers_situasjon:',
+    username: 'Flexjar ansvar denne uka',
+    text: 'Instruksjoner',
+    thread_ts: hovedpost.ts,
 })
