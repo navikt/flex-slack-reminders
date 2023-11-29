1d59a70464cb232c72e57bac0c7c2610a7179d54
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-29 15:03:01 +0100

    prodansvar

--- src/prodansvar.ts
+++ src/prodansvar.ts
@@ -1,30 +1,35 @@
 import './common/configInit'
 
-import { sendSlackMessage } from './common/slackPosting'
 import { prodansvarlig } from './common/prodansvarlig'
+import { slackWebClient } from './common/slackClient'
+import { flexDev } from './common/slackChannels'
 
 const ansvarlig = prodansvarlig()
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const blocks = [] as any[]
-
-blocks.push(
-    {
-        type: 'divider',
-    },
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `:male-police-officer: *Prodansvar denne uka*
-Det er <@${ansvarlig.memberId}> som har prodansvar denne uka.`,
+const hovedpost = await slackWebClient.chat.postMessage({
+    channel: flexDev(),
+    blocks: [
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `Det er <@${ansvarlig.memberId}> som har prodansvar denne uka.`,
+            },
         },
-    },
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `:link: *Nyttige lenker*
+    ],
+    icon_emoji: ':male-police-officer:',
+    username: 'Dagens prodansvar',
+    text: 'Dagens prodansvar',
+})
+
+await slackWebClient.chat.postMessage({
+    channel: flexDev(),
+    blocks: [
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `:link: *Nyttige lenker*
 <https://logs.adeo.no/goto/85280c10-59cf-11ed-8607-d590fd125f80|Logger flex namespace>
 <https://grafana.nais.io/d/H8jR8MFGk/flex-kafka-lag?orgId=1&refresh=5m&from=now-26h&to=now&var-datasource=prod-gcp|Flex kafka lag dashboardet>
 <https://grafana.nais.io/d/AdtqLupWk/flex-funksjonell?from=now-24h&to=now|Flex funksjonell>
@@ -32,8 +37,11 @@ Det er <@${ansvarlig.memberId}> som har prodansvar denne uka.`,
 <https://github.com/orgs/navikt/security/alerts/code-scanning?query=is:open%20team:flex|Code QL funn>
 <https://grafana.nais.io/d/CK_g1AWIk/flex-app-oversikt?orgId=1&var-datasource=prod-gcp&var-namespace=flex&var-app=All&var-environment=prod|Flex apper oversikt>
 <https://github.com/orgs/navikt/security/alerts/dependabot?q=is:open%20team:flex|Github sårbarheter>`,
+            },
         },
-    },
-)
-
-await sendSlackMessage('FLEX_DEV_WEBHOOK', { blocks })
+    ],
+    icon_emoji: ':male-police-officer:',
+    username: 'Dagens prodansvar',
+    text: 'Instruksjoner',
+    thread_ts: hovedpost.ts,
+})
