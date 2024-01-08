e3d521bdbc31c513ce0f92388a1ea8d3641883ac
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-08 15:40:39 +0100

    Flex prod

--- src/prodansvar.ts
+++ src/prodansvar.ts
@@ -33,7 +33,7 @@ await slackWebClient.chat.postMessage({
 <https://logs.adeo.no/goto/85280c10-59cf-11ed-8607-d590fd125f80|Logger flex namespace>
 <https://grafana.nav.cloud.nais.io/d/H8jR8MFGk/flex-kafka-lag?orgId=1&refresh=5m&from=now-26h&to=now&var-datasource=prod-gcp|Flex kafka lag dashboardet>
 <https://grafana.nav.cloud.nais.io/d/AdtqLupWk/flex-funksjonell?from=now-24h&to=now|Flex funksjonell>
-<https://grafana.nav.cloud.nais.io/d/AVwFIm0Mz/cloudsql-gcp?orgId=1&var-datasource=prod-gcp-stackdriver&var-project=nais-prod|CloudSQL - GCP>
+<https://grafana.nav.cloud.nais.io/d/AVwFIm0Mz/cloudsql-gcp?orgId=1&var-datasource=prod-gcp-stackdriver&var-project=flex-prod-af40|CloudSQL - GCP>
 <https://github.com/orgs/navikt/security/alerts/code-scanning?query=is:open%20team:flex|Code QL funn>
 <https://grafana.nav.cloud.nais.io/d/CK_g1AWIk/flex-app-oversikt?orgId=1&var-datasource=prod-gcp&var-namespace=flex&var-app=All&var-environment=prod|Flex apper oversikt>
 <https://github.com/orgs/navikt/security/alerts/dependabot?q=is:open%20team:flex|Github sårbarheter>`,
