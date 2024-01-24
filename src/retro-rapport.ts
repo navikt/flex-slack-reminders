74e2f709350eecec2081a0bcc809f6ed958168d7
golshidbahadorian <golshid.bahadorian@nav.no>
2024-01-24 14:16:24 +0100

    Bruker slack client i retro rapport

--- src/retro-rapport.ts
+++ src/retro-rapport.ts
@@ -1,7 +1,8 @@
 import './common/configInit'
 import * as dayjs from 'dayjs'
 
-import { sendSlackMessage } from './common/slackPosting'
+import { slackWebClient } from './common/slackClient'
+import { flexInternal } from './common/slackChannels'
 
 const startDate = dayjs('2023-10-11')
 const currentDate = dayjs()
@@ -12,21 +13,25 @@ const retroRapportLink = `https://flex-docs.ekstern.dev.nav.no/retro-board/retro
     currentWeekNumber + 1
 }--${dayjs().year()}`
 
-const blocks = [
-    {
-        type: 'section',
-        text: {
-            type: 'mrkdwn',
-            text: `:writing_hand: :page_with_curl: dette ble diskutert i <${retroRapportLink}|retro>${dayjs()}.`,
-        },
-    },
-]
-
 const ukeAdjustment = 1
 
 if ((currentWeekNumber + ukeAdjustment) % 2 === 0) {
     console.log('retro rapport')
-    await sendSlackMessage('FLEXINTERNAL_WEBHOOK', { blocks })
+    await slackWebClient.chat.postMessage({
+        channel: flexInternal(),
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: ` dette ble diskutert i <${retroRapportLink}|retro>${dayjs()}.`,
+                },
+            },
+        ],
+        icon_emoji: ':writing_hand: :page_with_curl:',
+        username: 'CodeQL alerts',
+        text: 'CodeQL',
+    })
 } else {
     console.log(`Ikke tid for retro rapportering fordi ukenummer pluss ${ukeAdjustment} ikke er delelig med 2`)
 }
