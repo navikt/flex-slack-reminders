d1ab0f2aa27cf2cb3208396ebb5ebcb29b5a84b1
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-09-11 23:48:14 +0200

    oppdaterte tekster for hver ukedag

--- src/prodansvar.ts
+++ src/prodansvar.ts
@@ -1,6 +1,6 @@
 import './common/configInit'
 
-import { prodansvarlig } from './common/prodansvarlig'
+import { dagTekst, prodansvarlig } from './common/prodansvarlig'
 import { slackWebClient } from './common/slackClient'
 import { flexProdansvar } from './common/slackChannels'
 
@@ -13,7 +13,7 @@ const hovedpost = await slackWebClient.chat.postMessage({
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: `Det er <@${ansvarlig.memberId}> som har prodansvar denne uka.`,
+                text: `Det er <@${ansvarlig.memberId}> som har prodansvar denne uka. Dagens oppgaver finnes i tråd :thread:`,
             },
         },
     ],
@@ -29,14 +29,7 @@ await slackWebClient.chat.postMessage({
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: `:link: *Nyttige lenker*
-<https://logs.adeo.no/goto/85280c10-59cf-11ed-8607-d590fd125f80|Logger flex namespace>
-<https://grafana.nav.cloud.nais.io/d/H8jR8MFGk/flex-kafka-lag?orgId=1&refresh=5m&from=now-26h&to=now&var-datasource=prod-gcp|Flex kafka lag dashboardet>
-<https://grafana.nav.cloud.nais.io/d/AdtqLupWk/flex-funksjonell?from=now-24h&to=now|Flex funksjonell>
-<https://grafana.nav.cloud.nais.io/d/AVwFIm0Mz/cloudsql-gcp?orgId=1&var-datasource=prod-gcp-stackdriver&var-project=flex-prod-af40|CloudSQL - GCP>
-<https://github.com/orgs/navikt/security/alerts/code-scanning?query=is:open%20team:flex|Code QL funn>
-<https://grafana.nav.cloud.nais.io/d/CK_g1AWIk/flex-app-oversikt?orgId=1&var-datasource=prod-gcp&var-namespace=flex&var-app=All&var-environment=prod|Flex apper oversikt>
-<https://github.com/orgs/navikt/security/alerts/dependabot?q=is:open%20team:flex|Github sårbarheter>`,
+                text: dagTekst(),
             },
         },
     ],
