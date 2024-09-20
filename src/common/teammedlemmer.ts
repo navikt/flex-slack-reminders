bb3039c2d957b99456911a2a36fca4164db2f33e
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-09-20 11:26:50 +0200

    felles liste for teammedlemmer, la til Marte

--- src/common/teammedlemmer.ts
+++ src/common/teammedlemmer.ts
@@ -1,18 +1,22 @@
-export const prodansvarlige = [
-    { initialer: 'OBL', memberId: 'U06FTG7L532' },
-    { initialer: 'NJM', memberId: 'U02AM04QV96' },
-    { initialer: 'HSA', memberId: 'UPU6U4H9R' },
-    { initialer: 'GB', memberId: 'U0124V94CLR' },
-    { initialer: 'ØK', memberId: 'U021UJDDMHB' },
+interface Flexer {
+    initialer: string
+    memberId: string
+    prodansvar?: boolean
+    flexjar?: boolean
+}
+
+export const alleFlexere: Flexer[] = [
+    { initialer: 'ATS', memberId: 'U04HMSMPTKK', flexjar: true },
+    { initialer: 'MV', memberId: 'U06QE5KETL2', flexjar: true },
+    { initialer: 'PB', memberId: 'UE89E1WTY', flexjar: true },
+    { initialer: 'TM', memberId: 'U06QFQVELJF', flexjar: true },
+    { initialer: 'GB', memberId: 'U0124V94CLR', prodansvar: true, flexjar: true },
+    { initialer: 'HSA', memberId: 'UPU6U4H9R', prodansvar: true, flexjar: true },
+    { initialer: 'NJM', memberId: 'U02AM04QV96', prodansvar: true, flexjar: true },
+    { initialer: 'OBL', memberId: 'U06FTG7L532', prodansvar: true, flexjar: true },
+    { initialer: 'ØK', memberId: 'U021UJDDMHB', prodansvar: true, flexjar: true },
 ]
 
-export const flexjaransvarlige = [
-    { initialer: 'OBL', memberId: 'U06FTG7L532' },
-    { initialer: 'NJM', memberId: 'U02AM04QV96' },
-    { initialer: 'HSA', memberId: 'UPU6U4H9R' },
-    { initialer: 'ØK', memberId: 'U021UJDDMHB' },
-    { initialer: 'GB', memberId: 'U0124V94CLR' },
-    { initialer: 'PB', memberId: 'UE89E1WTY' },
-    { initialer: 'TM', memberId: 'U06QFQVELJF' },
-    { initialer: 'ATS', memberId: 'U04HMSMPTKK' },
-]
+export const prodansvarlige: Flexer[] = alleFlexere.filter((member) => member.prodansvar)
+
+export const flexjaransvarlige: Flexer[] = alleFlexere.filter((member) => member.flexjar)
