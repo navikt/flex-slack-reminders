c905cb5b4d150d0eaa44576a77c9b8d97c038fef
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-12-04 10:57:42 +0100

    Gjør oddetallssjekk i TypeScript

--- src/friday-wins-reminder.ts
+++ src/friday-wins-reminder.ts
@@ -1,26 +1,33 @@
 import dayjs from 'dayjs'
 import weekOfYear from 'dayjs/plugin/weekOfYear'
+import './common/configInit'
 
 import { slackWebClient } from './common/slackClient'
 import { flexInternal } from './common/slackChannels'
 
 dayjs.extend(weekOfYear)
 
-await slackWebClient.chat.postMessage({
-    channel: flexInternal(),
-    blocks: [
-        {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: `:calendar: Det er onsdag i uke ${dayjs().week()}. Det er en oddtallsuke, noe som betyr at det er
-                Friday Wins denne uka! Har du noe har lyst til å vise frem? Noe du har laget eller noe du har kodet :meow_code:?
-                Noen tall og grafer, eller andre nyheter? Da er det helt sikkert lurt å begynne å forberede det nå
-                sånn at det ikke blir stress på fredag!`,
+if (dayjs().week() % 2 !== 0) {
+    await slackWebClient.chat.postMessage({
+        channel: flexInternal(),
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `:calendar: Det er uke ${dayjs().week()} og :alphabet-white-f::alphabet-white-r::alphabet-white-i::alphabet-white-d::alphabet-white-a::alphabet-white-y:  :alphabet-white-w::alphabet-white-i::alphabet-white-n::alphabet-white-s:!`,
+                },
             },
-        },
-    ],
-    icon_emoji: ':taco:',
-    username: 'Friday Wins',
-    text: 'Husk Friday Wins',
-})
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `Har du noe har lyst til å vise frem? Noe du har tegnet, laget eller kodet? Noen tall eller grafer? Da er det helt sikkert lurt å begynne å forberede det allerede nå!`,
+                },
+            },
+        ],
+        icon_emoji: ':taco:',
+        username: 'Friday Wins',
+        text: 'Husk Friday Wins',
+    })
+}
